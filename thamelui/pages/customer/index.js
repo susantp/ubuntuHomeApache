95f0a728e28e96c6c2f2830c1858f6d052bd3b5d
Susant Paudel <gracysusant@gmail.com>
2022-06-15 20:25:41 +0545

    ..

--- thamelui/pages/customer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../../layout/Layout'
-export default function Index() {
-  return (
-    <Layout>
-        {/* <h1>customer</h1> */}
-    </Layout>
-  )
-}
--- thamelui/store/atoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-
-export const themeState = atom({
-  key: 'sidebarWidthState',
-  default: 0
-})
